0b4cc8a960816c18989383786846a167ae4e8cfd
Zhipeng-Chang <zchang0302@gmail.com>
2019-05-07 14:54:40 -0600

    quick fix for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+try:
+    import django_heroku
+    django_heroku.settings(locals())
+
+except ImportError:
+    pass
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
